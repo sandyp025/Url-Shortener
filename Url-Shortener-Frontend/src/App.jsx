4c25d4fc74e09f0655fddf5e603f7e72d175754b
sandyp025 <sandipmandal02.sm@gmail.com>
2025-08-15 01:07:58 +0530

    feat: add Docker support, routing components, and UI updates

--- Url-Shortener-Frontend/src/App.jsx
+++ Url-Shortener-Frontend/src/App.jsx
@@ -1,31 +1,16 @@
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import LandingPage from './components/LandingPage';
-import AboutPage from './components/AboutPage';
-import RegisterPage from './components/RegisterPage';
-import Navbar from "./components/NavBar";
-import Footer from "./components/Footer";
-import LoginPage from './components/LoginPage';
-import { Toaster } from "react-hot-toast";
-import DashboardLayout from "./components/Dashboard/DashboardLayout";
+import { BrowserRouter as Router } from 'react-router-dom'
+import { getApps } from './utils/helper'
+
 function App() {
+
+  const CurrentApp = getApps();
+
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Toaster position="bottom-center" />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/Dashboard" element={<DashboardLayout />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <CurrentApp />
+    </Router>
   )
 }
 
-export default App
+export default App;
